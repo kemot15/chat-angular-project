71d7d3a55406c858362a068b363bc51222c182cf
Tomek <kemot15@tlen.pl>
2021-06-13 20:11:12 +0200

    routing configuration

--- Web/chat-angular-project/src/app/app-routing.module.ts
+++ Web/chat-angular-project/src/app/app-routing.module.ts
@@ -3,20 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
-        path: '',
-        loadChildren: () => import('./post/post.module').then(m => m.PostModule)
-    },
-    {
-        path: 'posts',
-        loadChildren: () => import('./post/post.module').then(m => m.PostModule)
-    },
-    {
         path: 'not-found', 
         loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule)
-    },
+    }, 
     {
         path: '**', redirectTo: 'not-found'
+    },
+    {
+        path: '',
+        loadChildren: () => import('./post/post.module').then(m => m.PostModule)
     }
+   
 ]
 
 @NgModule({
